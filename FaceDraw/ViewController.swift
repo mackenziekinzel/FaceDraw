12d3ab99f1373624f90d2970f77b063914bf2448
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-15 16:05:18 -0400

    create updateUI func in ViewController

--- FaceDraw/ViewController.swift
+++ FaceDraw/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var faceView: FaceView!
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
+    }
     
-    var expression = FacialExpression(eyes: .open, mouth: .grin)
+    var expression = FacialExpression(eyes: .open, mouth: .grin) {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.grin:0.5, .smile:1.0, .neutral: 0.0, .smirk:-0.5, .frown:-1.0]
+    
+    private func updateUI() {
+        
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
     
 }
+
 
