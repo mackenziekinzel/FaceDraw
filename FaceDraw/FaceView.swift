2d697a6a7a1dba653d5752f023a25822b6588a95
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-14 22:12:38 -0400

    add curve to pathForMouth

--- FaceDraw/FaceView.swift
+++ FaceDraw/FaceView.swift
@@ -12,6 +12,7 @@ class FaceView: UIView {
     
     var scale: CGFloat = 0.9
     var eyesOpen: Bool = false
+    var mouthCurvature: Double = 1.0 // 1.0 is full smile, -1.0 is full frown
     
     private var skullRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
@@ -44,7 +45,7 @@ class FaceView: UIView {
             path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
         } else {
             path = UIBezierPath()
-            path.move(to: CGPoint(x: eyeCenter.x, y: eyeCenter.y))
+            path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
             path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
         }
         
@@ -59,7 +60,17 @@ class FaceView: UIView {
         
         let mouthRect = CGRect(x: skullCenter.x - mouthWidth / 2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
         
-        let path = UIBezierPath(rect: mouthRect)
+        let smileOffset = CGFloat(max(-1, min(mouthCurvature, 1))) * mouthRect.height
+        
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY)
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+        
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 3, y: start.y + smileOffset)
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 3, y: start.y + smileOffset)
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
         path.lineWidth = 5.0
         return path
     }
