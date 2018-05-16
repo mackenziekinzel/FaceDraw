bedf20cb1be550b6d2a642b26ccce2677c8d8222
Mackenzie Kinzel <mkinzel2@kent.edu>
2018-05-16 17:51:40 -0400

    embed VC's in UINavigationControllers

--- FaceDraw/EmotionsViewController.swift
+++ FaceDraw/EmotionsViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 class EmotionsViewController: UIViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destinationViewController = segue.destination
+        var destinationViewController = segue.destination
+        if let navigationController = destinationViewController as? UINavigationController {
+            destinationViewController = navigationController.visibleViewController ?? destinationViewController
+        }
         if let faceViewController = destinationViewController as? FaceViewController,
             let identifier = segue.identifier,
             let expression = emotionalFaces[identifier] {
                     faceViewController.expression = expression
+            faceViewController.navigationItem.title = (sender as? UIButton)?.currentTitle
             }
         }
     
